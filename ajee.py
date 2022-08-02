015c8a5c17579ac183e1aa89889bff3ac0d59a40
Aje2006 <110461663+Aje2006@users.noreply.github.com>
2022-08-02 15:56:19 +0530

    Add files via upload

---
+++ ajee.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import os
+
+
+# In[6]:
+
+
+os.getcwd()
+
+
+# In[7]:
+
+
+import pandas as pd
+
+
+# In[8]:
+
+
+pd.read_excel("book1.xlsx")
+
+
+# In[ ]:
+
+
+
+
